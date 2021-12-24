b1333f0566bc6b8b27c4c8a377262aefe01fea5b
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-24 23:15:57 +0000

    Cleaned up and fixed game_mode.rs and map.rs

--- src/map.rs
+++ src/map.rs
@@ -1,9 +1,17 @@
-use rsfml::system::{Vector2i, Vector2f};
+use rsfml::system::{Vector2f, Vector2i};
 
-#[deriving(Clone)]
 pub struct Map {
-    map : Vec<i32>,
-    map_size : Vector2i
+    map: Vec<i32>,
+    map_size: Vector2i,
+}
+
+impl Clone for Map {
+    fn clone(&self) -> Self {
+        Self {
+            map: self.map.clone(),
+            map_size: self.map_size.clone(),
+        }
+    }
 }
 
 enum Orientation {
@@ -14,41 +22,46 @@ enum Orientation {
     Top_Left,
     Top_Right,
     Bottom_Left,
-    Bottom_Right
+    Bottom_Right,
 }
 
 impl Map {
-    pub fn new<'r>(map : Vec<i32>, map_size : &'r Vector2f) -> Map {
+    pub fn new<'r>(map: Vec<i32>, map_size: &'r Vector2f) -> Map {
         Map {
-            map : map,
-            map_size : Vector2i { x : map_size.x as i32, y : map_size.y as i32 }
+            map,
+            map_size: Vector2i {
+                x: map_size.x as i32,
+                y: map_size.y as i32,
+            },
         }
     }
 
-    pub fn get_block_with_orientation<'r>(&self,
-                                          block_orientation : Orientation,
-                                          position : &'r Vector2i) -> Option<i32> {
+    pub fn get_block_with_orientation<'r>(
+        &self,
+        block_orientation: Orientation,
+        position: &'r Vector2i,
+    ) -> Option<i32> {
         match block_orientation {
-            Top     => self.handle_top(position),
-            Bottom  => self.handle_bottom(position),
-            Left    => self.handle_left(position),
-            Right   => self.handle_right(position),
-            Top_Left    => self.handle_top_left(position),
-            Top_Right   => self.handle_top_right(position),
-            Bottom_Left     => self.handle_bottom_left(position),
-            Bottom_Right    => self.handle_bottom_right(position)
+            Top => self.handle_top(position),
+            Bottom => self.handle_bottom(position),
+            Left => self.handle_left(position),
+            Right => self.handle_right(position),
+            Top_Left => self.handle_top_left(position),
+            Top_Right => self.handle_top_right(position),
+            Bottom_Left => self.handle_bottom_left(position),
+            Bottom_Right => self.handle_bottom_right(position),
         }
     }
 
-    pub fn get_block<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        if position.x < 0 ||
-           position.y < 0 ||
-           position.x > self.map_size.x ||
-           position.y > self.map_size.y {
+    pub fn get_block<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        if position.x < 0
+            || position.y < 0
+            || position.x > self.map_size.x
+            || position.y > self.map_size.y
+        {
             return None;
-        }
-        else {
-            return Some(self.map[(position.y * self.map_size.x + position.x) as uint]);
+        } else {
+            return Some(self.map[(position.y * self.map_size.x + position.x) as usize]);
         }
     }
 
@@ -56,43 +69,67 @@ impl Map {
         &self.map_size
     }
 
-    fn handle_top<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x - 1, y : position.y };
+    fn handle_top<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x - 1,
+            y: position.y,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_bottom<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x + 1, y : position.y };
+    fn handle_bottom<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x + 1,
+            y: position.y,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_left<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x, y : position.y - 1};
+    fn handle_left<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x,
+            y: position.y - 1,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_right<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x, y : position.y + 1};
+    fn handle_right<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x,
+            y: position.y + 1,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_top_left<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x - 1, y : position.y - 1};
+    fn handle_top_left<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x - 1,
+            y: position.y - 1,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_top_right<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x - 1, y : position.y + 1};
+    fn handle_top_right<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x - 1,
+            y: position.y + 1,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_bottom_left<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x + 1, y : position.y - 1};
+    fn handle_bottom_left<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x + 1,
+            y: position.y - 1,
+        };
         self.get_block(&tmp_pos)
     }
 
-    fn handle_bottom_right<'r>(&self, position : &'r Vector2i) -> Option<i32> {
-        let tmp_pos = Vector2i { x : position.x + 1, y : position.y + 1};
+    fn handle_bottom_right<'r>(&self, position: &'r Vector2i) -> Option<i32> {
+        let tmp_pos = Vector2i {
+            x: position.x + 1,
+            y: position.y + 1,
+        };
         self.get_block(&tmp_pos)
     }
 }
