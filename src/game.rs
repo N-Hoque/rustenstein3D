3cc1bb6b71996268d28124f3da12976b01da25f5
N-Hoque <Naimul_Hoque@outlook.com>
2021-12-24 23:17:23 +0000

    Cleaned up and fixed game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,75 +1,81 @@
 #![allow(non_snake_case)]
 
-use FPS::*;
 use event_handler::*;
 use game_mode::*;
 use texture_loader::TextureLoader;
+use FPS::*;
 
-use rsfml::graphics::{RenderWindow, RenderTarget, Font, Color};
-use rsfml::window::keyboard;
+use rsfml::{
+    graphics::{Color, Font, RenderTarget, RenderWindow},
+    window::Key,
+};
 
 pub struct GameLoop<'s> {
-    render_window : RenderWindow,
-    fps_handler : Option<FPSHandler<'s>>,
-    event_handler : EventHandler,
-    clear_color : Color,
-    game_mode : GameMode<'s>,
-    texture_loader : &'s TextureLoader
+    render_window: RenderWindow,
+    fps_handler: Option<FPSHandler<'s>>,
+    event_handler: EventHandler,
+    clear_color: Color,
+    game_mode: GameMode<'s>,
+    texture_loader: &'s TextureLoader,
 }
 
 impl<'s> GameLoop<'s> {
-    pub fn new(render_window : RenderWindow,
-               texture_loader : &'s TextureLoader,
-               noground : bool) -> GameLoop<'s> {
-        let tmp_size = render_window.get_size();
+    pub fn new(
+        render_window: RenderWindow,
+        texture_loader: &'s TextureLoader,
+        noground: bool,
+    ) -> GameLoop<'s> {
+        let tmp_size = render_window.size();
         GameLoop {
-            render_window : render_window,
-            fps_handler : None,
-            event_handler : EventHandler::new(),
-            clear_color : Color::new_RGB(3, 64, 59),
-            game_mode : GameMode::new(tmp_size, texture_loader, noground),
-            texture_loader : texture_loader
+            render_window,
+            texture_loader,
+            fps_handler: None,
+            event_handler: EventHandler::new(),
+            clear_color: Color::rgb(3, 64, 59),
+            game_mode: GameMode::new(tmp_size, texture_loader, noground),
         }
     }
 
-    pub fn activate_FPS(&mut self, 
-                        font : &'s Font) -> (){
+    pub fn activate_FPS(&mut self, font: &'s Font) {
         match self.fps_handler {
-            Some(_)     => (),
-            None        => self.fps_handler = Some(FPSHandler::new(font))
+            Some(_) => (),
+            None => self.fps_handler = Some(FPSHandler::new(font)),
         }
     }
 
-    pub fn deactivate_FPS(&mut self) -> () {
+    pub fn deactivate_FPS(&mut self) {
         match self.fps_handler {
-            Some(_)     => self.fps_handler = None,
-            None        => ()
+            Some(_) => self.fps_handler = None,
+            None => (),
         }
     }
 
-    pub fn run(&mut self) -> () {
+    pub fn run(&mut self) {
         while self.render_window.is_open() {
             self.update();
             self.draw();
         }
     }
 
-    pub fn update(&mut self) -> () {
+    pub fn update(&mut self) {
         self.event_handler.update_events(&mut self.render_window);
-        if self.event_handler.has_closed_event() ||
-           self.event_handler.is_key_pressed(keyboard::Escape) {
+        if self.event_handler.has_closed_event() || self.event_handler.is_key_pressed(Key::ESCAPE) {
             self.render_window.close();
         }
         self.game_mode.update(&self.event_handler);
         self.fps_handler.as_mut().unwrap().update();
     }
 
-    pub fn draw(&mut self) -> () {
-        self.render_window.clear(&self.clear_color);
+    pub fn draw(&mut self) {
+        self.render_window.clear(self.clear_color);
         self.game_mode.draw(&mut self.render_window);
         match self.fps_handler {
-            Some(_)     => self.fps_handler.as_mut().unwrap().draw(&mut self.render_window),
-            None        => {}
+            Some(_) => self
+                .fps_handler
+                .as_mut()
+                .unwrap()
+                .draw(&mut self.render_window),
+            None => {}
         };
         self.render_window.display();
     }
