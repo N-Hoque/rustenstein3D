3e71cc23cee8f68f0e0a8fb9f500bfc9dabfedf4
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-25 19:09:59 +0000

    Removed docstrings from fps.rs

--- src/FPS.rs
+++ src/FPS.rs
@@ -1,33 +1,15 @@
-/*!
-* Basic class to display the current game frames per seconds
-*
-* This class display the current fps in the left bottom of the window.
-*
-*/
-
 use rsfml::{
     graphics::{Color, Font, RenderTarget, RenderWindow, Text, Transformable},
     system::{Clock, Vector2f},
 };
 
-/// Definition of class FPSHandler
 pub struct FPSHandler<'s> {
     fps_clock: Clock,
     text: Text<'s>,
     images: usize,
 }
 
-/// Implementation of class FPSHandler
 impl<'s> FPSHandler<'s> {
-    /**
-     * Constructor of class FPSHandler
-     *
-     * # Arguments
-     * * render_window - The RenderWindow to calculate the FPS
-     * * font - The font to render the text on the window.
-     *
-     * Return a new instance of FPSHandler
-     */
     pub fn new(font: &'s Font) -> FPSHandler<'s> {
         let mut text = Text::new("0", font, 20);
         text.set_position(Vector2f::new(10., 10.));
@@ -39,12 +21,6 @@ impl<'s> FPSHandler<'s> {
         }
     }
 
-    /**
-     * Update internal data of the FPSHandler
-     *
-     * Call this function at each end of the loop to update
-     * FPSHandler internal data.
-     */
     pub fn update(&mut self) {
         if self.fps_clock.elapsed_time().as_seconds() >= 0.33 {
             self.text.set_string(&(self.images * 3).to_string());
@@ -54,9 +30,6 @@ impl<'s> FPSHandler<'s> {
         self.images += 1;
     }
 
-    /**
-     * Draw the current FPS on the left bottom of the window
-     */
     pub fn draw(&self, render_window: &mut RenderWindow) {
         render_window.draw(&self.text)
     }
