466e7e6dd216e1ed9cb4eb6a300af62ff21cfd73
Naimul Hoque <Naimul_Hoque@outlook.com>
2021-12-25 21:36:47 +0000

    Vector calculations have been simplified in mini_map/logic.rs

--- src/mini_map/logic.rs
+++ src/mini_map/logic.rs
@@ -13,8 +13,10 @@ use super::MiniMap;
 
 impl MiniMap {
     pub fn new(map: Map, window_size: Vector2u) -> MiniMap {
-        let mut mini_map_view = View::new(Vector2f::default(), Vector2f::default());
-        mini_map_view.set_size(Vector2f::new(window_size.x as f32, window_size.y as f32));
+        let mut mini_map_view = View::new(
+            Vector2f::default(),
+            Vector2f::new(window_size.x as f32, window_size.y as f32),
+        );
         mini_map_view.set_viewport(&FloatRect::new(0.70, 0.05, 0.25, 0.25));
         mini_map_view.set_rotation(-90.0);
 
@@ -40,10 +42,7 @@ impl MiniMap {
         self.player_pos = player_position;
         let map_view = (*self.mini_map_view).borrow_mut();
         map_view.rotate(new_rotation);
-        map_view.set_center(Vector2f::new(
-            self.player_pos.x * 80.,
-            self.player_pos.y * 80.,
-        ));
+        map_view.set_center(self.player_pos * 80.);
         self.rotation += new_rotation;
     }
 
@@ -54,8 +53,8 @@ impl MiniMap {
         let mut rect = RectangleShape::with_size(Vector2f::new(80., 80.));
         rect.set_fill_color(Color::rgba(255, 255, 255, 175));
         render_window.set_view(&self.mini_map_view);
-        let mut pos = Vector2i::new(0, 0);
         let map_size = self.map.get_map_size();
+        let mut pos = Vector2i::default();
         while pos.x < map_size.x {
             while pos.y < map_size.y {
                 let block = self
@@ -63,7 +62,7 @@ impl MiniMap {
                     .get_block(pos)
                     .unwrap_or_else(|| panic!("Getting block in minimap at position: {:?}", pos));
                 rect.set_texture(texture_loader.get_texture(block), false);
-                rect.set_position(Vector2f::new(pos.x as f32 * 80., pos.y as f32 * 80.));
+                rect.set_position(Vector2f::new(pos.x as f32, pos.y as f32) * 80.);
                 render_window.draw(&rect);
                 pos.y += 1;
             }
@@ -72,10 +71,7 @@ impl MiniMap {
         }
         rect.set_fill_color(Color::rgba(255, 0, 0, 125));
         rect.set_origin(Vector2f::new(40., 40.));
-        rect.set_position(Vector2f::new(
-            self.player_pos.x as f32 * 80.,
-            self.player_pos.y as f32 * 80.,
-        ));
+        rect.set_position(self.player_pos * 80.);
         render_window.draw(&rect);
         render_window.set_view(&def_view);
     }
